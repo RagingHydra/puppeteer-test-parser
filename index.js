16f96de699833887d0ada4205ba6dc135a1aa29f
Maksim Sidorychev <maksimsidorychev0@gmail.com>
2024-10-04 23:31:51 +0300

    implement selectRegion

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ import minimist from "minimist";
 const log = console.log;
 const argv = minimist(process.argv.slice(2));
 
-const url = argv['url'] || 'https://vprok.ru/product/domik-v-derevne-dom-v-der-moloko-ster-3-2-950g--309202';
+const url = argv['url'] || 'https://www.vprok.ru/product/domik-v-derevne-dom-v-der-moloko-ster-3-2-950g--309202';
 const region = argv['region'] || 'Санкт-Петербург и область';
-const regionSelector = argv['regS'] || 'Region_region__6OUBn';
-const screenshotWidth = 1080;
-const screenshotHeight = 1024;
 
 const selectRegion = async (page, region) => {
-    throw new Error('Not implemented');
+    await page.evaluate(() => {
+        document.querySelector('.Region_region__6OUBn').click();
+    });
+    
+    const regionList = await page.waitForSelector('.UiRegionListBase_list__cH0fK');
+    const regionHandle = await regionList.$(`::-p-text(${region})`);
+
+    return regionHandle.click();
 }
 
 const saveScreenshot = async page => {
@@ -23,11 +27,27 @@ const saveProduct = async page => {
 }
 
 const start = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+         headless: true, 
+         defaultViewport: null, 
+         args: ['--start-maximized'] 
+    });
+    log('Puppeteer init');
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    page.setDefaultTimeout(5000);
+
+    await page.waitForSelector('#__next'); //Waiting through 'Checking your browser' screen
+    await Promise.any([ //Infinite loading fix in case of connection problems
+        page.waitForNetworkIdle(),
+        new Promise(r => setTimeout(r, 5000))
+    ]);
+    log('Page loaded');
     
     await selectRegion(page, region);
+    log('Region selected');
+
     await saveScreenshot(page);
     await saveProduct(page);
 
